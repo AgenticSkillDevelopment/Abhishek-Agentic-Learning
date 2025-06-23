f2db018cf0eed2e0ff26be732e8c1d1ad96db52b
Abhishek <abhishek.singh@wittybrains.com>
2025-06-23 08:37:18 +0530

    updated

---
+++ Countstring/server.py
@@ -0,0 +1,10 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="count_string",host="127.0.0.1",port=8000,timeout=30)
+@mcp.tool()
+def count_string(string:str)->int:
+    if not isinstance(string,str):
+        raise ValueError("Input must be a string")
+    return string.lower().count("a")
+if __name__ == "__main__":
+    mcp.run()
